769f27c641d04db79cffc7d6fa330bfbccf14718
PrashilAryal <prashilaryal13@gmail.com>
2022-11-14 23:59:35 +0545

    add, read and delete features

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Models\todo;
+
+class TodoController extends Controller
+{
+    public function index(){
+        return view('welcome', ['list' => todo::all()]);
+    }
+
+    public function saveTodo(Request $req){
+        $todoObj = new todo();
+        $todoObj->title = $req->todo;
+
+        $todoObj->save();
+
+        //save data to the database
+
+        return redirect('/');
+    }
+    public function destroy($id){
+        $data=todo::find($id);
+        $data->delete();
+        echo "Record deleted successfully.";
+        return redirect('/');
+    }
+}
